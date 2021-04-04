822643f531b8d45acddd3ba747555a5961b02cc6
velya <velya.96@gmail.com>
2021-04-04 13:46:12 +0300

    Code style fix

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,7 @@
 package hexlet.code;
 
 public class App {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
