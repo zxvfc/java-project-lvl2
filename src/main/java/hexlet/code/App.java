c61d30c918cded19ff2e1c84be6f4f03fca1dbc2
velya <velya.96@gmail.com>
2021-04-04 14:02:55 +0300

    Added picocli and base functionality about it

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+
+import static picocli.CommandLine.*;
+import static picocli.CommandLine.Option;
+
+@Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
 public class App {
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    private boolean help;
+
+    @Option(names = { "-v", "--version" }, description = "Print version information and exit.")
+    private boolean printVersion;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new CommandLine(new App()).execute(args);
     }
 }
